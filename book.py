3ad0317172e22dc6a6ff112d49222e94b7822d93
aaravtech271 <135567156+aaravtech271@users.noreply.github.com>
2023-08-30 16:12:11 -0400

    Create book.py

---
+++ book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Price: ${self.price}"
